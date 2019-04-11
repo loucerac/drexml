4dc02f3ac04099070af1e4d5811257558b68a435
Carlos <loucerac@unican.es>
2019-04-11 12:54:18 +0200

    Add funtion to plot feature improtances.

---
+++ src/learn.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import numpy  as np
+import matplotlib.pyplot as plt
+
+from xgboost.core     import XGBoostError
+from lightgbm.sklearn import LightGBMError
+
+
+def plot_feature_importances(clf, X_train, y_train=None, 
+                             top_n=10, figsize=(8,8), print_table=False, 
+                             title="Feature Importances"):
+    '''
+    plot feature importances of a tree-based sklearn estimator
+    
+    Note: X_train and y_train are pandas DataFrames
+    
+    Note: Scikit-plot is a lovely package but I sometimes have issues
+              1. flexibility/extendibility
+              2. complicated models/datasets
+          But for many situations Scikit-plot is the way to go
+          see https://scikit-plot.readthedocs.io/en/latest/Quickstart.html
+    
+    Parameters
+    ----------
+        clf         (sklearn estimator) if not fitted, this routine will fit it
+        
+        X_train     (pandas DataFrame)
+        
+        y_train     (pandas DataFrame)  optional
+                                        required only if clf has not already been fitted 
+        
+        top_n       (int)               Plot the top_n most-important features
+                                        Default: 10
+                                        
+        figsize     ((int,int))         The physical size of the plot
+                                        Default: (8,8)
+        
+        print_table (boolean)           If True, print out the table of feature importances
+                                        Default: False
+        
+    Returns
+    -------
+        the pandas dataframe with the features and their importance
+    '''
+    
+    __name__ = "plot_feature_importances"
+    
+    try: 
+        if not hasattr(clf, 'feature_importances_'):
+            clf.fit(X_train.values, y_train.values.ravel())
+
+            if not hasattr(clf, 'feature_importances_'):
+                raise AttributeError("{} does not have feature_importances_ attribute".
+                                    format(clf.__class__.__name__))
+                
+    except (XGBoostError, LightGBMError, ValueError):
+        clf.fit(X_train.values, y_train.values.ravel())
+            
+    feat_imp = pd.DataFrame({'importance':clf.feature_importances_})    
+    feat_imp['feature'] = X_train.columns
+    feat_imp.sort_values(by='importance', ascending=False, inplace=True)
+    feat_imp = feat_imp.iloc[:top_n]
+    
+    feat_imp.sort_values(by='importance', inplace=True)
+    feat_imp = feat_imp.set_index('feature', drop=True)
+    feat_imp.plot.barh(title=title, figsize=figsize)
+    plt.xlabel('Feature Importance Score')
+    plt.show()
+    
+    if print_table:
+        from IPython.display import display
+        print("Top {} features in descending order of importance".format(top_n))
+        display(feat_imp.sort_values(by='importance', ascending=False))
+        
+    return feat_imp
