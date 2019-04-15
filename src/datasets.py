3e628ac6c2fb006412294243f4907e54d477db3b
Carlos <carlos.loucera@juntadeandalucia.es>
2019-04-15 10:23:46 +0200

    Fix bug while indexing by circuit name.

--- src/datasets.py
+++ src/datasets.py
@@ -101,7 +101,7 @@ def get_disease_data(disease):
     # Filter data
     target_gene_ids = gene_metadata.index[gene_metadata.approved_targets]
     gene_expression = gene_expression[target_gene_ids]
-    disease_circuits = path_metadata.loc[path_metadata.in_desease]
+    disease_circuits = path_metadata.loc[path_metadata.in_disease].index
     pathvals = pathvals.loc[:, disease_circuits]
 
 
