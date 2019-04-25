9ccd85df3ec35af11ddb4e27aa33910afcf1d63d
Carlos <carlos.loucera@juntadeandalucia.es>
2019-04-25 13:36:45 +0200

    Add docstrings to datasets source code file.

--- src/datasets.py
+++ src/datasets.py
@@ -5,7 +5,7 @@ Author: Carlos Loucera <carlos.loucera@juntadeandalucia.es>
 Author: Maria Pena Chilet <maria.pena.chilet.ext@juntadeandalucia.es>
 Author: Marina Esteban <marina.estebanm@gmail.com>
 
-Basic IO functionallity for Achilles cell line predictions.
+Basic IO functionallity for HORD project.
 """
 
 import sys
@@ -40,7 +40,18 @@ clinical_info_fname = "clinical_info_gtex.rds.feather"
 
 
 def get_circuits_fname(disease):
+    """Get circuits metadata file name based on the disease under study.
 
+    Parameters
+    ----------
+    disease : str
+        The disease under study.
+
+    Returns
+    -------
+    str
+        Circuits metadata file name.
+    """
     if disease == "fanconi":
         circuits_fname = "circuits_FA.rds.feather"
     else:
@@ -49,7 +60,18 @@ def get_circuits_fname(disease):
     return circuits_fname
 
 def get_pathvals_fname(disease):
+    """Get pathvals file name based on the disease under study.
 
+    Parameters
+    ----------
+    disease : str
+        The disease under study.
+
+    Returns
+    -------
+    str
+        Pathvals file name.
+    """
     if disease == "fanconi":
         pathvals_fname = "expreset_pathvals_FA.rds.feather"
     else:
@@ -58,7 +80,18 @@ def get_pathvals_fname(disease):
     return pathvals_fname
 
 def load_circuits(disease):
+    """Load ciruicts metadata for the disease under study.
 
+    Parameters
+    ----------
+    disease : str
+        The disease under study.
+
+    Returns
+    -------
+    DataFrame
+        Ciruits DataFrame.
+    """
     circuits_fname = get_circuits_fname(disease)
 
     circuits = read_feather(DATA_PATH.joinpath(circuits_fname))
@@ -70,6 +103,13 @@ def load_circuits(disease):
     return circuits
 
 def load_genes():
+    """Load gene metadata dataset.
+
+    Returns
+    -------
+    DataFrame, [n_features, ]
+        Gene metadata dataset.
+    """
 
     genes = read_feather(DATA_PATH.joinpath(genes_fname))
     genes.set_index("index", drop=True, inplace=True)
@@ -79,7 +119,13 @@ def load_genes():
     return genes
 
 def load_expression():
+    """Load gene expression dataset.
 
+    Returns
+    -------
+    DataFrame, [n_samples, n_features]
+        Gene expression dataset (Gtex).
+    """
     expression = read_feather(DATA_PATH.joinpath(expression_fname))
     expression.columns = expression.columns.str.replace("X", "")
     expression.set_index("index", drop=True, inplace=True)
@@ -88,7 +134,13 @@ def load_expression():
     return expression
 
 def load_pathvals(disease):
+    """Load pathvals dataset.
 
+    Returns
+    -------
+    DataFrame, [n_samples, n_features]
+        Pathvals dataset (Gtex).
+    """
     pathvals_fname = get_pathvals_fname(disease)
     pathvals = read_feather(DATA_PATH.joinpath(pathvals_fname))
     pathvals.set_index("index", drop=True, inplace=True)
@@ -101,7 +153,22 @@ def load_pathvals(disease):
     return pathvals
 
 def get_disease_data(disease, pathways=None):
+    """Load all datasets for a given dataset.
 
+    Parameters
+    ----------
+    disease : str
+        Disease under study.
+    pathways : array like, str, [n_pathways, ]
+        Pathways to use as ML targets, by default None refers to circuits given
+        by in_disease column in ciruits metadata dataset.
+
+    Returns
+    -------
+    DataFrame, DataFrame, DataFrame, DataFrame, DataFrame
+        Gene expression, pathvals, circuit metadata, gene metadata, clinical
+        metadata
+    """
     # Load data
     gene_exp = load_expression()
     pathvals = load_pathvals(disease)
@@ -112,12 +179,12 @@ def get_disease_data(disease, pathways=None):
     # test integrity and reorder by sample index
 
     gene_exp, pathvals = test_integrity(
-        gene_exp, 
-        pathvals, 
+        gene_exp,
+        pathvals,
         "Gene expr. and Pathvals")
     gene_exp, clinical_info = test_integrity(
-        gene_exp, 
-        clinical_info, 
+        gene_exp,
+        clinical_info,
         "Gene expr. and Clinical data")
 
     # Filter data
@@ -135,6 +202,13 @@ def get_disease_data(disease, pathways=None):
     return gene_exp, pathvals, path_metadata, gene_metadata, clinical_info
 
 def load_clinical_data():
+    """Load clinical metadata dataset.
+
+    Returns
+    -------
+    DataFrame, [n_samples, n_features]
+        Clinical metadata dataset (Gtex).
+    """
     clinical_info_path = DATA_PATH.joinpath(clinical_info_fname)
     clinical_info = read_feather(clinical_info_path)
     clinical_info.set_index("index", drop=True, inplace=True)
@@ -143,6 +217,22 @@ def load_clinical_data():
     return clinical_info
 
 def test_integrity(x, y, msg):
+    """Test combined dataset integrity.
+
+    Parameters
+    ----------
+    x : DataFrame
+        DataFrame to comapre.
+    y : DataFrame
+        DataFrame to comapre.
+    msg : str
+        Message to be printed.
+
+    Returns
+    -------
+    DataFrame, DataFrame
+        DataFrames reordered by a common index.
+    """
     if not np.array_equal(x.index.shape, y.index.shape):
         print(x.index.shape, y.index.shape)
         sys.exit(msg + " sample index differ in shape.")
@@ -153,4 +243,3 @@ def test_integrity(x, y, msg):
         sys.exit(msg + " different sample indexes.")
 
     return x, y
-
