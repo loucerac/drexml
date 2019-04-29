4b39101eab9a70bd9d47be3da1a61eeae8281220
Carlos <carlos.loucera@juntadeandalucia.es>
2019-04-29 19:57:25 +0200

    Create output folder in sh.

--- hord.sh
+++ hord.sh
@@ -9,9 +9,32 @@ PATHWAY1="hsa03460m"
 PATHWAY2="hsa04110"
 GSET="all"
 
-JOB_NAME="hord_${DISEASE}_${MODE}_all_${GSET}"
+NAME="all"
+OUT_FOLDER="${OUT_PATH}/${DISEASE}/${NAME}/${GSET}${MLMODEL}/${OPT}/${MODE}/${SEED}"
+mkdir -p ${OUT_FOLDER}
+git archive -o code_snapshot.zip HEAD
+zip -rv code_snapshot.zip .env
+mv code_snapshot.zip "${OUT_FOLDER}/code_snapshot.zip"
+echo "Code snapshot saved to ${OUT_FOLDER}"
+JOB_NAME="hord_${DISEASE}_${MODE}_${NAME}_${GSET}"
 sbatch -J ${JOB_NAME} --export=DISEASE=${DISEASE},MLMODEL=${MLMODEL},OPT=${OPT},SEED=${SEED},MODE=${MODE},GSET=${GSET} ${BATCH_FILE}
+
+NAME="${PATHWAY1}_${PATHWAY2}"
+OUT_FOLDER="${OUT_PATH}/${DISEASE}/${NAME}/${GSET}/${MLMODEL}/${OPT}/${MODE}/${SEED}"
+mkdir -p ${OUT_FOLDER}
+git archive -o code_snapshot.zip HEAD
+zip -rv code_snapshot.zip .env
+mv code_snapshot.zip "${OUT_FOLDER}/code_snapshot.zip"
+echo "Code snapshot saved to ${OUT_FOLDER}"
 JOB_NAME="hord_${DISEASE}_${MODE}_${PATHWAY1}_${PATHWAY2}_${GSET}"
 sbatch -J ${JOB_NAME} --export=DISEASE=${DISEASE},MLMODEL=${MLMODEL},OPT=${OPT},SEED=${SEED},MODE=${MODE},PATHWAY1=${PATHWAY1},PATHWAY2=${PATHWAY2},GSET=${GSET} ${BATCH_FILE}
+
+NAME="${PATHWAY1}"
+OUT_FOLDER="${OUT_PATH}/${DISEASE}/${NAME}/${GSET}/${MLMODEL}/${OPT}/${MODE}/${SEED}"
+mkdir -p ${OUT_FOLDER}
+git archive -o code_snapshot.zip HEAD
+zip -rv code_snapshot.zip .env
+mv code_snapshot.zip "${OUT_FOLDER}/code_snapshot.zip"
+echo "Code snapshot saved to ${OUT_FOLDER}"
 JOB_NAME="hord_${DISEASE}_${MODE}_${PATHWAY1}_${GSET}"
 sbatch -J ${JOB_NAME} --export=DISEASE=${DISEASE},MLMODEL=${MLMODEL},OPT=${OPT},SEED=${SEED},MODE=${MODE},PATHWAY1=${PATHWAY1},GSET=${GSET} ${BATCH_FILE}
--- hord_local.sh
+++ hord_local.sh
@@ -11,11 +11,12 @@ GSET="all"
 
 export $(egrep -v '^#' .env | xargs)
 
-OUT_FOLDER=$(python hord.py --disease ${DISEASE} --mlmodel ${MLMODEL} --opt ${OPT} --seed ${SEED} --mode ${MODE} --gset ${GSET})
-OUT_FOLDER=$(echo ${OUT_FOLDER} | awk '{print $NF}')
-
+NAME="all"
+OUT_FOLDER="${OUT_PATH}/${DISEASE}/${NAME}/${GSET}/${MLMODEL}/${OPT}/${MODE}/${SEED}"
+mkdir -p OUT_FOLDER
 git archive -o code_snapshot.zip HEAD
 zip -rv code_snapshot.zip .env
 mv code_snapshot.zip "${OUT_FOLDER}/code_snapshot.zip"
-
 echo "Code snapshot saved to ${OUT_FOLDER}"
+JOB_NAME="hord_${DISEASE}_${MODE}_${NAME}_${GSET}"
+python hord.py --disease ${DISEASE} --mlmodel ${MLMODEL} --opt ${OPT} --seed ${SEED} --mode ${MODE} --gset ${GSET}
