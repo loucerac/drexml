a81b1f7c5998d895de256a93b58f25180444ea8a
Carlos <loucerac@unican.es>
2019-04-29 20:14:34 +0200

    Use .tar.gz instead of zip for ubiquitousness.

--- hord.sh
+++ hord.sh
@@ -15,9 +15,11 @@ export $(egrep -v '^#' .env | xargs)
 NAME="all"
 OUT_FOLDER="${OUT_PATH}/${DISEASE}/${NAME}/${GSET}${MLMODEL}/${OPT}/${MODE}/${SEED}"
 mkdir -p ${OUT_FOLDER}
-git archive -o code_snapshot.zip HEAD
-zip -rv code_snapshot.zip .env
-mv code_snapshot.zip "${OUT_FOLDER}/code_snapshot.zip"
+git archive -o code_snapshot.tar.gz HEAD
+gunzip -f code_snapshot.tar.gz
+tar -uf code_snapshot.tar .env
+gzip code_snapshot.tar
+mv code_snapshot.tar.gz "${OUT_FOLDER}/code_snapshot.tar.gz"
 echo "Code snapshot saved to ${OUT_FOLDER}"
 JOB_NAME="hord_${DISEASE}_${MODE}_${NAME}_${GSET}"
 sbatch -J ${JOB_NAME} --export=DISEASE=${DISEASE},MLMODEL=${MLMODEL},OPT=${OPT},SEED=${SEED},MODE=${MODE},GSET=${GSET} ${BATCH_FILE}
@@ -25,9 +27,11 @@ sbatch -J ${JOB_NAME} --export=DISEASE=${DISEASE},MLMODEL=${MLMODEL},OPT=${OPT},
 NAME="${PATHWAY1}_${PATHWAY2}"
 OUT_FOLDER="${OUT_PATH}/${DISEASE}/${NAME}/${GSET}/${MLMODEL}/${OPT}/${MODE}/${SEED}"
 mkdir -p ${OUT_FOLDER}
-git archive -o code_snapshot.zip HEAD
-zip -rv code_snapshot.zip .env
-mv code_snapshot.zip "${OUT_FOLDER}/code_snapshot.zip"
+git archive -o code_snapshot.tar.gz HEAD
+gunzip -f code_snapshot.tar.gz
+tar -uf code_snapshot.tar .env
+gzip code_snapshot.tar
+mv code_snapshot.tar.gz "${OUT_FOLDER}/code_snapshot.tar.gz"
 echo "Code snapshot saved to ${OUT_FOLDER}"
 JOB_NAME="hord_${DISEASE}_${MODE}_${PATHWAY1}_${PATHWAY2}_${GSET}"
 sbatch -J ${JOB_NAME} --export=DISEASE=${DISEASE},MLMODEL=${MLMODEL},OPT=${OPT},SEED=${SEED},MODE=${MODE},PATHWAY1=${PATHWAY1},PATHWAY2=${PATHWAY2},GSET=${GSET} ${BATCH_FILE}
@@ -35,9 +39,11 @@ sbatch -J ${JOB_NAME} --export=DISEASE=${DISEASE},MLMODEL=${MLMODEL},OPT=${OPT},
 NAME="${PATHWAY1}"
 OUT_FOLDER="${OUT_PATH}/${DISEASE}/${NAME}/${GSET}/${MLMODEL}/${OPT}/${MODE}/${SEED}"
 mkdir -p ${OUT_FOLDER}
-git archive -o code_snapshot.zip HEAD
-zip -rv code_snapshot.zip .env
-mv code_snapshot.zip "${OUT_FOLDER}/code_snapshot.zip"
+git archive -o code_snapshot.tar.gz HEAD
+gunzip -f code_snapshot.tar.gz
+tar -uf code_snapshot.tar .env
+gzip code_snapshot.tar
+mv code_snapshot.tar.gz "${OUT_FOLDER}/code_snapshot.tar.gz"
 echo "Code snapshot saved to ${OUT_FOLDER}"
 JOB_NAME="hord_${DISEASE}_${MODE}_${PATHWAY1}_${GSET}"
 sbatch -J ${JOB_NAME} --export=DISEASE=${DISEASE},MLMODEL=${MLMODEL},OPT=${OPT},SEED=${SEED},MODE=${MODE},PATHWAY1=${PATHWAY1},GSET=${GSET} ${BATCH_FILE}
