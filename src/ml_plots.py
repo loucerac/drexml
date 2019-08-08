eb267c9ced1ad2427a5777256fdf2dc44b9ff83e
Carlos <carlos.loucera@juntadeandalucia.es>
2019-08-08 14:11:59 +0200

    Addd option in ml plots to not use  dictionaries.

--- src/ml_plots.py
+++ src/ml_plots.py
@@ -6,7 +6,9 @@ from pathlib import Path
 
 plt.style.use('fivethirtyeight')
 
-model_rel_fpath = "globalrel_symbol.tsv"
+use_circuit_dict = False
+
+model_rel_fpath = "model_global_relevance.tsv"
 model_rel = pd.read_csv(model_rel_fpath, sep="\t", index_col=0)
 
 path = "shap_values_task_relevance.tsv"
@@ -44,12 +46,12 @@ for stat in cv_stats.keys():
             df = pd.DataFrame(cv_stats[stat][split], columns=circuit_ids)
             dfs.append(df)
 
-fname = "name_circuits.tsv"
-circuit_names = pd.read_csv(fname, sep="\t", index_col=1)
-
 to_plot2 = (dfs[0] - (1- dfs[1])/2).copy()
-to_plot2.columns = circuit_names.loc[to_plot2.columns, "NAME"]
-to_plot2.head()
+
+if use_circuit_dict:
+    fname = "name_circuits.tsv"
+    circuit_names = pd.read_csv(fname, sep="\t", index_col=1)
+    to_plot2.columns = circuit_names.loc[to_plot2.columns, "NAME"]
 
 dfs[0].columns = to_plot2.columns
 dfs[1].columns = to_plot2.columns
