2bcdcc17a7ef5b315d8f3e50038bdeb595024f90
Carlos <carlos.loucera@juntadeandalucia.es>
2020-01-15 13:36:26 +0100

    Add  bash scripts to plot in hpc.

--- plot.sh
+++ plot.sh
@@ -1,16 +1,4 @@
-module load python/3.7.3
-module load python373-matplotlib
-module load python373-pandas
-module load python373-scikit-learn
-module load python373-scipy
-module load python373-pyarrow
-module load python373-shap
-module load python373-xlrd
-module load python373-xlwt
-
-source ~/.pyenv/holrd/bin/activate
-
-BASE="mnt/lustre/scratch/CBRA/research/projects/holrd/experiments/"
+BASE="/mnt/lustre/scratch/CBRA/research/projects/holrd/experiments/"
 
 declare -a DISEASES
 DISEASES=("ALB" "EVC" "H" "RP" "WS")
@@ -22,8 +10,9 @@ for DISEASE in "${DISEASES[@]}"
 do
     for EXPERIMENT in "${EXPERIMENTS[@]}"
     do
+        JOBNAME="plot_${DISEASE}_${EXPERIMENT}"
         MLPATH=$BASE/${DISEASE}/${EXPERIMENT}/ml/morf_train
-        python src/ml_plots.py $MLPATH 0 1
+        sbtach -J ${JOBNAME} --export=MLPATH=$MLPATH plot.sbatch
     done
    # or do whatever with individual element of the array
 done
