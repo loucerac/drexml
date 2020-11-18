fa55bf870ccf2e9c160ac08d1010beaa2602728b
loucerac <carlos.loucera@juntadeandalucia.es>
2020-11-18 10:45:31 +0100

    Fix joblib import.

--- src/ml_plots.py
+++ src/ml_plots.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
-from sklearn.externals import joblib
+import joblib
 
 exts = ["pdf", "png", "eps", "svg"]
 
