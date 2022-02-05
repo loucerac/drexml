6b377b4e7ed5b32a04e8d2f5bc9cdc57c60bf5df
Carlos Loucera <carlos.loucera@junatdeandalucia.es>
2022-02-05 19:11:35 +0100

    Add setup method.

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -6,7 +6,9 @@ Author: Marina Esteban <marina.estebanm@gmail.com>
 
 Unit testing for datasets module.
 """
-
+import tempfile
+from pathlib import Path
+import shutil
 try:
     import importlib.resources as pkg_resources
 except ImportError:
@@ -25,7 +27,15 @@ def get_disease_path():
     """
     with pkg_resources.path("dreml.resources", "experiment.env") as f:
         data_file_path = f
-    return data_file_path
+    return Path(data_file_path)
+
+def prepare_disease():
+    """Prepare fake disease data folder."""
+    tmp_dir = Path(tempfile.mkdtemp())
+    disease_path_in = get_disease_path()
+    disease_path_out = tmp_dir.joinpath(disease_path_in)
+    # Use as posix to make it compatible with python<=3.7
+    shutil.copy(disease_path_in.as_posix(), disease_path_out.as_posix())
 
 
 def test_get_disease_data():
