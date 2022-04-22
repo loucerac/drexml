fbee2a7f92b51fee6b0e775ea6c40d151a63dad5
Carlos Loucera <carlos.loucera@juntadeandalucia.es>
2022-04-22 10:18:16 +0200

    Fix aesthetics.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,17 +14,17 @@ import os
 import sys
 import sphinx_rtd_theme
 
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath("../.."))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'dreml'
-copyright = '2022, Carlos Loucera'
-author = 'Carlos Loucera'
+project = "dreml"
+copyright = "2022, Carlos Loucera"
+author = "Carlos Loucera"
 
 # The full version, including alpha/beta/rc tags
-release = '0.9.4'
+release = "0.9.4"
 
 
 # -- General configuration ---------------------------------------------------
@@ -36,18 +36,18 @@ master_doc = "index"
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
-    'sphinx.ext.intersphinx',
-    'sphinx_click',
-    'sphinx_rtd_theme',
+    "sphinx.ext.intersphinx",
+    "sphinx_click",
+    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -55,9 +55,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
