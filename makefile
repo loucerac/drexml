c07adad433f731c0ec5f4312c01427c207d4e6f1
Carlos Loucera <carlos.loucera@juntadeandalucia.es>
2023-03-14 19:39:31 +0100

    Add conditional tests based on gpu status.

--- makefile
+++ makefile
@@ -1,12 +1,28 @@
 .PHONY: install test format
+.ONESHELL:
+
+SHELL = /bin/bash
+CONDA_ACTIVATE = source $$(conda info --base)/etc/profile.d/conda.sh ; conda activate
+
 all: install format test
+
 install:
-	conda create -p ./.venv --override-channels -c "nvidia/label/cuda-11.8.0" -c conda-forge cuda cuda-nvcc cuda-toolkit gxx=11.2 python=3.10
+ifeq ($(use_gpu),1)
+	conda create -y -p ./.venv --override-channels -c "nvidia/label/cuda-11.8.0" -c conda-forge cuda cuda-nvcc cuda-toolkit gxx=11.2 python=3.10
+else
+	conda create -y -p ./.venv --override-channels -c conda-forge python=3.10
+endif
+	$(CONDA_ACTIVATE) ./.venv
 	poetry install
 	poetry run pytest
 format:
+	$(CONDA_ACTIVATE) ./.venv
 	poetry run isort drexml tests
 	poetry run black drexml tests
 	(cd docs && poetry run make html)
 test:
-	tox
+ifeq ($(use_gpu),1)
+	nox -- "gpu"
+else
+	nox
+endif
--- noxfile.py
+++ noxfile.py
@@ -1,18 +1,23 @@
+import nox
 from nox_poetry import session
 
 
-@session(python=["3.10", "3.9", "3.8"], venv_backend="conda")
+@session(venv_backend="conda")
+@nox.parametrize("python", ["3.10", "3.9", "3.8"])
 def tests(session):
-    session.conda_install(
-        "cuda",
-        "cuda-nvcc",
-        "cuda-toolkit",
-        "gxx=11.2",
-        "-c",
-        "nvidia/label/cuda-11.8.0",
-        "-c",
-        "conda-forge",
-        "--override-channels",
-    )
+    if session.posargs:
+        if any(["gpu" in arg for arg in session.posargs]):
+            session.conda_install(
+                "cuda",
+                "cuda-nvcc",
+                "cuda-toolkit",
+                "gxx=11.2",
+                "-c",
+                "nvidia/label/cuda-11.8.0",
+                "-c",
+                "conda-forge",
+                "--override-channels",
+            )
+ 
     session.install("pytest", ".")
     session.run("pytest")
