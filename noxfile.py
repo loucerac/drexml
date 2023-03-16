5e171e032ae0f6e3d9fe0ea6a18be20d4f16e69c
Carlos Loucera <carlos.loucera@juntadeandalucia.es>
2023-03-16 07:09:07 +0100

    Add doctrings.

--- noxfile.py
+++ noxfile.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Test suite.
+"""
+
 import nox
 from nox_poetry import session
 
@@ -5,6 +10,8 @@ from nox_poetry import session
 @session(venv_backend="conda")
 @nox.parametrize("python", ["3.10", "3.9", "3.8"])
 def tests(session):
+    """ Test with conda.
+    """
     if session.posargs:
         if any(["gpu" in arg for arg in session.posargs]):
             session.conda_install(
