3666f1032be357d942a350647e760df120bd29d8
Carlos Loucera <carlos.loucera@juntadeandalucia.es>
2023-07-31 17:37:45 +0200

    Scale by max abs value across circuits.

--- drexml/shap_scores_filtering.py
+++ drexml/shap_scores_filtering.py
@@ -3,6 +3,7 @@
 SHAP scores analysis for DREXML.
 """
 import pandas as pd
+from sklearn.preprocessing import maxabs_scale
 
 
 def process_data(data_folder, stability_threshold=0.6):
@@ -17,43 +18,37 @@ def process_data(data_folder, stability_threshold=0.6):
     Returns:
     None. The final dataframe is saved as a .tsv file.
     """
-    # Read the .tsv files into a DataFrames
-    shap_values = pd.read_csv(
-        f"{data_folder}/shap_summary_symbol.tsv", sep="\t", index_col=0
-    )
+
+    # Read the .tsv files into a DataFrames (circuits X genes)
+    shap_values = pd.read_csv(f"{data_folder}/shap_summary.tsv", sep="\t", index_col=0)
     shap_selection = pd.read_csv(
-        f"{data_folder}/shap_selection_symbol.tsv", sep="\t", index_col=0
+        f"{data_folder}/shap_selection.tsv", sep="\t", index_col=0
     )
+
+    # Read the .tsv files into a DataFrames (circuits X metrics)
     stability = pd.read_csv(
-        f"{data_folder}/stability_results_symbol.tsv", sep="\t", index_col=0
+        f"{data_folder}/stability_results.tsv", sep="\t", index_col=0
     )
 
-    # Define min and max range for normalization
-    min_range = -1
-    max_range = 1
+    shap_selection = shap_selection.loc[:, shap_selection.any()]
 
-    # Apply Min-Max normalization to each row
-    shap_normalized = shap_values.copy()
-    shap_normalized.iloc[:, 1:] = shap_normalized.iloc[:, 1:].apply(
-        lambda row: (row - row.min())
-        / (row.max() - row.min())
-        * (max_range - min_range)
-        + min_range,
-        axis=1,
+    circuits_stable = stability[
+        stability["stability"] > stability_threshold
+    ].index.intersection(shap_values.index)
+
+    # circuit-wise scale by max abs value
+    # filter using the selected genes (columns)
+    # filter using stable circuits
+    filtered_df_stable = (
+        shap_values.transform(maxabs_scale)
+        .multiply(shap_selection)
+        .dropna(axis=1)
+        .loc[circuits_stable]
     )
 
-    shap_filtered_norm = shap_normalized * shap_selection
-    filtered_df = shap_filtered_norm.loc[:, (shap_filtered_norm != 0).sum() != 0]
-
-    # Now filter those circuits that have stability above the desired threshold
-    stability_00 = stability[stability["stability"] > stability_threshold]
-
-    stability_00.reset_index(inplace=True)
-    filtered_df.reset_index(inplace=True)
-
-    filtered_df_stable = filtered_df[
-        filtered_df["circuit_name"].isin(stability_00["circuit_name"])
-    ]
     filtered_df_stable.to_csv(
-        f"{data_folder}/shap_filtered_stability_symbol.tsv", sep="\t", index=False
+        f"{data_folder}/shap_filtered_stability_symbol.tsv",
+        sep="\t",
+        index=True,
+        index_label="circuit_name",
     )
