5c9013e882a9cec94abc3b1cfb5561730e81a5c7
loucerac <loucerac@users.noreply.github.com>
2024-02-05 08:34:58 +0000

    Deploying to gh-pages from @ loucerac/drexml@0f03d7f03c965933674ddf1deb08dd58884dec69 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.0+editable',
+    VERSION: '1.0.3+editable',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
