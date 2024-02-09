574a55dc567f10c1c7000a02139566c24fb2c828
Carlos Loucera <carlos.loucera@juntadeandalucia.es>
2024-02-09 22:02:05 +0100

    Fix job name.

--- examples/benchmark/02_run.sh
+++ examples/benchmark/02_run.sh
@@ -15,5 +15,5 @@ declare -a map_size_lst=( 1 25 50 75 100)
 for i in "${map_size_lst[@]}"; do
 	this_i=$(printf "%03d\n" "$i")
 	cd $BASEDIR/experiments/disease_${this_i}
-	sbatch -J -n 50 --mem 100g  --wrap="${CONDA_RUN} hyperfine --runs 10 --shell=bash --export-csv disease_${this_i}.csv 'drexml run disease.env'" -o log.out -e log.err
+	sbatch -J drexml-b${this_i} -n 50 --mem 100g  --wrap="${CONDA_RUN} hyperfine --runs 10 --shell=bash --export-csv disease_${this_i}.csv 'drexml run disease.env'" -o log.out -e log.err
 done
