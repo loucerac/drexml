843758e6eff417f3dad6003205a41b294e33843d
Carlos Loucera <carlos.loucera@juntadeandalucia.es>
2024-02-09 12:55:17 +0100

    Fix experiment folder calling.

--- examples/benchmark/run.sh
+++ examples/benchmark/run.sh
@@ -12,9 +12,11 @@ rm -rf ./.venv
 
 if [ $USE_GPU == 1 ]; then
 	conda create -y -p ./.venv --override-channels -c "nvidia/label/cuda-11.8.0" \
-	-c conda-forge cuda cuda-nvcc cuda-toolkit gxx=11.2 python=3.10
+	-c conda-forge \
+	cuda cuda-nvcc cuda-toolkit gxx=11.2 python=3.10 python-dotenv jupyterlab hyperfine
 else
-	conda create -y -p ./.venv --override-channels -c conda-forge python=3.10
+	conda create -y -p ./.venv --override-channels -c conda-forge \
+	python=3.10 python-dotenv jupyterlab hyperfine
 fi
 
 # install latest stable version fo drexml
@@ -24,6 +26,8 @@ ${CONDA_RUN} pip install -I --force-reinstall --no-cache-dir --no-binary=shap dr
 if [ $USE_GPU == 1 ]; then
 	${CONDA_RUN} python -c 'import shap; shap.utils.assert_import("cext_gpu")'
 fi
+
+${CONDA_RUN} python 01_build_benchmark.py
 
 # run drexml using all CPUs and no GPUs
 #${CONDA_RUN} drexml run --n-gpus 0 experiment.env > results/drexml.out 2> results/drexml.err
