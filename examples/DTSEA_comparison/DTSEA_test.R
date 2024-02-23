c9db82f2fc087d269285d7e6e61e27568ef412e7
marinaEM <marina.estebanm@gmail.com>
2024-02-23 18:39:50 +0100

    seeds-reproducibility RWR

--- examples/DTSEA_comparison/DTSEA_test.R
+++ examples/DTSEA_comparison/DTSEA_test.R
@@ -14,7 +14,7 @@ if (!require(pacman, quietly = TRUE)){
 
 
 library(pacman)
-p_load(magrittr, dplyr, BiocManager, devtools, here, ggplot2)
+p_load(magrittr, dplyr, BiocManager, devtools, here, ggplot2, tibble)
 
 
 # Install the required packages for DTSEA and DisGeNet
@@ -51,14 +51,15 @@ colnames(drug_targets)<- gsub("drugbank_id", "drug_id", colnames(drug_targets)) 
 ##############
 # Perform a simple DTSEA analysis using default optional parameters then sort
 # the result dataframe by normalized enrichment scores (NES)
+set.seed(234)
 result_FA <- DTSEA(network = graph,
                 disease = FA_signature$Gene,
                 drugs = drug_targets, verbose = FALSE) %>% arrange(desc(NES))
 
 relevantFA_results <- select(result_FA, -leadingEdge) %>% arrange(desc(NES)) %>%  filter(NES > 0 & padj < .01)
-NES_FAdrug_targets <- relevantFA_results[,c(1,2,6)] %>%  add_column(target = drug_targets$gene_target[match(relevantFA_results$drug_id, drug_targets$drug_id)]) %>%  
+NES_FAdrug_targets <- relevantFA_results[,c(1,3,6)] %>%  add_column(target = drug_targets$gene_target[match(relevantFA_results$drug_id, drug_targets$drug_id)]) %>%  
   add_column(drug = drug_targets$drug_name[match(relevantFA_results$drug_id, drug_targets$drug_id)]) 
-length(unique(NES_FAdrug_targets$target)) ## 54 unique targets
+length(unique(NES_FAdrug_targets$target)) ## 52 unique targets
 
 
 ############
@@ -66,14 +67,15 @@ length(unique(NES_FAdrug_targets$target)) ## 54 unique targets
 ##############
 # Perform a simple DTSEA analysis using default optional parameters then sort
 # the result dataframe by normalized enrichment scores (NES)
+set.seed(234)
 result_FM <- DTSEA(network = graph,
                    disease = FM_signature$symbol,
                    drugs = drug_targets, verbose = FALSE) %>% arrange(desc(NES))
 
 relevantFM_results <- select(result_FM, -leadingEdge) %>% arrange(desc(NES)) %>%  filter(NES > 0 & padj < .01)
-NES_FMdrug_targets <- relevantFM_results[,c(1,2,6)] %>%  add_column(target = drug_targets$gene_target[match(relevantFM_results$drug_id, drug_targets$drug_id)]) %>%  
+NES_FMdrug_targets <- relevantFM_results[,c(1,3,6)] %>%  add_column(target = drug_targets$gene_target[match(relevantFM_results$drug_id, drug_targets$drug_id)]) %>%  
   add_column(drug = drug_targets$drug_name[match(relevantFM_results$drug_id, drug_targets$drug_id)])
-length(unique(NES_FMdrug_targets$target)) ## 85 unique targets
+length(unique(NES_FMdrug_targets$target)) ## 91 unique targets
  
 
 ###############################
@@ -87,12 +89,12 @@ length(unique(NES_FMdrug_targets$target)) ## 85 unique targets
 ### 1. Load relevant filtered drexml results 
 FA_drexml<- read.delim(gzfile(here("examples", "fanconi_anemia" , "results","shap_filtered_stability_symbol.tsv.gz"))) %>% column_to_rownames("circuit_name")
 
-overlapFA <- colnames(FA_drexml)[colnames(FA_drexml) %in% NES_FAdrug_targets$target] ## 7 overlap
+overlapFA <- colnames(FA_drexml)[colnames(FA_drexml) %in% NES_FAdrug_targets$target] ## 6 overlap
 
 ### 2. Rank targets in NES_FAdrug_targets based on p-value
 nes_fa_ranks <- NES_FAdrug_targets  %>%
   mutate(rank_nes_fa = row_number()) %>%
-  select(drug_id, drug ,target, pval, NES, rank_nes_fa) %>%
+  select(drug_id, drug ,target, padj, NES, rank_nes_fa) %>%
   filter(target %in% overlapFA)
 
 ### 3. Calculate mean absolute SHAP values and count relevant circuits for all targets
@@ -109,7 +111,7 @@ FA_stats_df <- as.data.frame(t(FA_stats), stringsAsFactors = FALSE) %>%
 
 ### 4. Create summarisation table
 
-summary_DTSEAcomparisonFA <- merge(nes_fa_ranks, FA_stats_df, by = "target", all.x = TRUE)  %>% .[order(.$pval,decreasing = F),]
+summary_DTSEAcomparisonFA <- merge(nes_fa_ranks, FA_stats_df, by = "target", all.x = TRUE)  %>% .[order(.$padj,decreasing = F),]
 write.table( summary_DTSEAcomparisonFA, here("examples", "DTSEA_comparison" ,"summary_DTSEAcomparisonFA.tsv"), sep="\t", quote = F, row.names = F, col.names = T)
 
 ### 5. Compare with cmap results
@@ -130,7 +132,7 @@ overlapFM <- colnames(FM_drexml)[colnames(FM_drexml) %in% NES_FMdrug_targets$tar
 ### 2. Rank targets in NES_FAdrug_targets based on p-value
 nes_fm_ranks <- NES_FMdrug_targets  %>%
   mutate(rank_nes_fm = row_number()) %>%
-  select(drug_id, drug ,target, pval, NES, rank_nes_fm) %>%
+  select(drug_id, drug ,target, padj, NES, rank_nes_fm) %>%
   filter(target %in% overlapFM)
 
 ### 3. Calculate mean absolute SHAP values and count relevant circuits for all targets
@@ -147,7 +149,7 @@ FM_stats_df <- as.data.frame(t(FM_stats), stringsAsFactors = FALSE) %>%
 
 ### 4. Create summarisation table
 
-summary_DTSEAcomparisonFM <- merge(nes_fm_ranks, FM_stats_df, by = "target", all.x = TRUE) %>% .[order(.$pval,decreasing = F),]
+summary_DTSEAcomparisonFM <- merge(nes_fm_ranks, FM_stats_df, by = "target", all.x = TRUE) %>% .[order(.$padj,decreasing = F),]
 write.table( summary_DTSEAcomparisonFM, here("examples", "DTSEA_comparison" ,"summary_DTSEAcomparisonFM.tsv"), sep="\t" ,quote = F, row.names = F, col.names = T)
 
 ### 5. Compare with cmap results
