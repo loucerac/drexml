f4b43209853346867da56611a51bc6251340dfe0
Carlos Loucera <carlos.loucera@juntadeandalucia.es>
2024-11-28 20:53:41 +0100

    Add script for normalizing gene expression data.

---
+++ scripts/hipathia/normalize_gexp.R
@@ -0,0 +1,46 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+
+gtex_fname <- file.path(args[1])
+vers <- args[2]
+output <- file.path(args[3])
+
+#########################################
+### Processing GTEx V8 datasets #####
+#########################################
+
+library(hipathia)
+library(feather)
+library(edgeR)
+library(data.table)
+library("R.utils")
+
+save_feather <- function(x, path) {
+  df <- data.frame(index = row.names(x), x)
+
+  write_feather(df, path)
+}
+
+AnnotationHub::setAnnotationHubOption("ASK", FALSE)
+
+# Load  RNA-seq expression set from GTEx *.gct file.
+
+## Read the downloaded GTEx raw counts dataset
+expreset_raw <- fread(
+  file = gtex_fname,
+  header = T, sep = "\t"
+) %>% as.data.frame(.)
+
+rownames(expreset_raw) <- expreset_raw$Name
+expreset_raw <- expreset_raw[, -(1:2)]
+
+# Normalization by TMM with "edgeR" package
+dge <- DGEList(counts = expreset_raw)
+tmm <- calcNormFactors(dge, method = "TMM")
+logcpm <- cpm(tmm, prior.count = 3, log = TRUE)
+trans_data <- translate_data(logcpm, "hsa")
+
+save_feather(
+  trans_data,
+  output
+)
